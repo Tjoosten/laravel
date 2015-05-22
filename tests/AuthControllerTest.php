4a2c2aacf9bd918e5f7eeffa5da4b324dc122d80
Tjoosten <Tjoosten3@gmail.com>
2015-05-22 16:58:24 +0200

    work on unit tests

--- tests/AuthControllerTest.php
+++ tests/AuthControllerTest.php
@@ -2,9 +2,27 @@
 
 class AuthControllerTest extends TestCase {
 
-	public function testBasicExample() {
+	public function testBaseUrl() {
 		$response = $this->call('GET', '/');
 		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+	public function testBlock() {
+
+	}
+
+	public function testUndoBlock() {
+
+	}
+
+	public function testSetToUser() {
+		$response = $this->call('GET', '');
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+	public function testSetToAdministrator() {
+		$response = $this->call('GET', '/');
+		$this->assertEquals(200, $response->getStatusCode())
 	}
 
 }
--- tests/DownloadMethodsTest.php
+++ tests/DownloadMethodsTest.php
@@ -1,1 +1,25 @@
 <?php
+
+class DownloadMethodsTest extends TestCase {
+
+	public function testExcelAll() {
+		$response = $this->call('GET', '/downloads/excel/all');
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+	public function testExcelSpecific() {
+		$response = $this->call('GET', '/downloads/excel/8');
+		$this->assertEquals(200, $response->getStatusCode()); 
+	}
+
+	public function testPdfAll() {
+		$response = $this->call('GET', '/downloads/pdf/all');
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+	public function testPdfSpecific() {
+		$response = $this->call('GET', '/downloads/pdf/1');
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+}
