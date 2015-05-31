37c11077ef73d500a04bacb2d649e73ef71ba015
Tjoosten <Tjoosten3@gmail.com>
2015-05-31 23:15:55 +0200

    fixed #4

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -3,14 +3,16 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
-class UserTableSeeder extends Seeder {
+class UserTableSeeder extends Seeder
+{
 
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-    DB::table('users')->truncate();
-	}
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->truncate();
+    }
+}
